37db008757777ecf9fef28c9b259b7e0f37e3eed
achrafbmr <143760360+AchrafBmr@users.noreply.github.com>
2024-03-08 23:49:03 +0100

    added show file

---
+++ resources/views/Employes/show.blade.php
@@ -0,0 +1,17 @@
+@extends('Employes.layout')
+
+@section('title' , 'Show Employes')
+
+@section('content')
+    <h2 style="padding: 15px">Employee ID: {{$emp['id']}}</h2>
+    <div class="card border-success mb-5" style="max-width: 25%; margin-left:37.5%; box-shadow: 5px 10px #888888;">
+        <div class="card-body">
+            <h6 class="card-title">Nom:</h6>
+            <p class="card-text text-center">{{$emp['nom']}}</p>
+            <h6 class="card-title">Prenom:</h6>
+            <p class="card-text text-center">{{$emp['prenom']}}</p>
+            <h6 class="card-title text-right">Email:</h6>
+            <p class="card-text text-center">{{$emp['email']}}</p>
+        </div>
+    </div>
+@stop
